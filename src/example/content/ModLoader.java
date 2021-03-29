18452b79acbe2599b6955e399caabed0c69e1eef
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-03-29 23:42:48 +0900

    Update ModLoader.java

--- src/example/content/ModLoader.java
+++ src/example/content/ModLoader.java
@@ -3,7 +3,9 @@ package example.content;
 import mindustry.ctype.ContentList;
 
 public class ModLoader implements ContentList {
-    private final ContentList[] contents = {};
+    private final ContentList[] contents = {
+            new ExItems()
+    };
     public void load(){
         for(ContentList list : contents){
             list.load();
