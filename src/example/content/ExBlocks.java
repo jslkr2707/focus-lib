7ff715beb4a1c2eb8dede361fd254ab5c89db111
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-03-29 23:39:12 +0900

    Create ExBlocks.java

---
+++ src/example/content/ExBlocks.java
@@ -0,0 +1,19 @@
+package example.content;
+
+import mindustry.ctype.ContentList;
+import rhino.ast.Block;
+
+public class ExBlocks implements ContentList {
+    public static Block
+    //stone age
+    firepit;
+
+    @Override
+    public void load(){
+        /*
+        firepit = new heatMulti("firepit", 2){{
+            heatCapacity = 400;
+            addRecipe
+         */
+    }
+}
