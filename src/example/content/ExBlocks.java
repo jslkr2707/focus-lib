1211806b7958ded96aeff41236a4f9eefe6149b4
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-03-31 23:48:34 +0900

    Update ExBlocks.java

--- src/example/content/ExBlocks.java
+++ src/example/content/ExBlocks.java
@@ -1,19 +1,30 @@
 package example.content;
 
+import example.world.blocks.heatMulti;
+import mindustry.content.Items;
 import mindustry.ctype.ContentList;
-import rhino.ast.Block;
+import multilib.Recipe;
+
+import static mindustry.type.ItemStack.with;
 
 public class ExBlocks implements ContentList {
-    public static Block
+    public static heatMulti
     //stone age
     firepit;
 
     @Override
     public void load(){
-        /*
         firepit = new heatMulti("firepit", 2){{
-            heatCapacity = 400;
-            addRecipe
-         */
+            isSmelter = true;
+
+            addRecipe(
+                    new Recipe.InputContents(with(ExItems.tree, 1)),
+                    new Recipe.OutputContents(with(Items.coal, 1)), 60f
+            );
+            addRecipe(
+                    new Recipe.InputContents(with(ExItems.tin, 1, Items.copper, 3)),
+                    new Recipe.OutputContents(with(ExItems.bronze, 2)), 120f
+            );
+        }};
     }
 }
