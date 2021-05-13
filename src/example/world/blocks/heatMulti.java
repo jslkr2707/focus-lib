cc52c1bf8faacc8abb85855a6a5fb30bfcc60715
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-05-13 11:41:30 +0900

    h

--- src/example/world/blocks/heatMulti.java
+++ src/example/world/blocks/heatMulti.java
@@ -2,6 +2,8 @@ package example.world.blocks;
 
 import arc.Core;
 import arc.graphics.g2d.Draw;
+import example.content.ExItems;
+import mindustry.content.Items;
 import mindustry.graphics.Pal;
 import mindustry.type.ItemStack;
 import mindustry.ui.Bar;
@@ -26,12 +28,18 @@ public class heatMulti extends MultiCrafter {
     }
 
     public class heatMultiBuild extends MultiCrafterBuild{
-        public float heat, heatCapacity, timer;
-        /* just default scl*/
+        public float heat = 0;
+        public float timer;
+        public int a;
+        public float calorie = 0;
         public float heatScl = 1f;
+        public ItemStack fuel;
+        public ItemStack[] inputStack;
+        /* just default scl*/
+        public float heatCap = 200f;
+        public int recLen = recs.length;
         public boolean isheated;
 
-        public float t1 =  heatCapacity / heatScl;
         @Override
         public void draw(){
             Draw.rect(Core.atlas.find(name+"-heat"), x, y);
@@ -41,19 +49,28 @@ public class heatMulti extends MultiCrafter {
 
         @Override
         public void updateTile(){
-            timer += 1f;
+            timer += 0.033f;
             isheated = false;
-            for (Recipe rec : recs) {
-                ItemStack[] inputStack = rec.input.items;
-                if (items.has(inputStack)) isheated = true;
+            for (int i = 0; i < recLen; i++) {
+                inputStack = recs[i].input.items;
+                for (int j = 0; j < inputStack.length; j++) {
+                    if (inputStack[j].item == ExItems.tree || inputStack[j].item == Items.coal) {
+                        fuel = inputStack[j];
+                        a = j;
+                        break;
+                    }
+                }
+                if (items.has(inputStack) && fuel != null) isheated = true;
             }
-            if (isheated && heat < heatCapacity && timer <= t1) {
-                heat += -Math.pow(timer, 2) + 2*t1*timer;
+            calorie += 200 * (fuel != null ? fuel.amount : 0);
+            if (isheated && heat <= heatCap && calorie > 0) {
+                heat += heatScl * timer;
+                calorie -= 0.2f;
+
+            }else{
+                heat = -0.09f;
             }
 
-            if (!isheated){
-                heat -= 0.3f;
-            }
             applyBoost((heat+1)/400, 60f);
         }
     }
