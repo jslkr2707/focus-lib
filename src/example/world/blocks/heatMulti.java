6e133a6bc5cf4518e540a767409adfed59bd8dd4
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-05-14 22:33:39 +0900

    h

--- src/example/world/blocks/heatMulti.java
+++ src/example/world/blocks/heatMulti.java
@@ -1,7 +1,6 @@
 package example.world.blocks;
 
 import arc.Core;
-import arc.graphics.g2d.Draw;
 import arc.util.Log;
 import arc.util.Strings;
 import mindustry.graphics.Pal;
@@ -13,7 +12,6 @@ import multilib.Recipe;
 public class heatMulti extends MultiCrafter {
     public heatMulti(String name, int recLen){
         super(name, recLen);
-        hasItems = true;
     }
     public heatMulti(String name, Recipe[] recs){
         super(name, recs);
@@ -24,21 +22,20 @@ public class heatMulti extends MultiCrafter {
         super.setBars();
 
         bars.add("heat", (heatMulti.heatMultiBuild e) -> new Bar(() ->
-                Core.bundle.format("bar.poweroutput", Strings.fixed(e.heat * 300, 1)), () -> Pal.lightishOrange, () -> e.heat));
-        bars.add("process", (heatMulti.heatMultiBuild h) -> new Bar("bar.process", Pal.powerBar, () -> h.process));
+                Core.bundle.format("bar.heat", Strings.fixed(e.heat * 400, 1)), () -> Pal.lightishOrange, () -> e.heat));
+        bars.add("process", (heatMulti.heatMultiBuild h) -> new Bar(() ->
+                Core.bundle.format("bar.progress", Strings.fixed((h.Hprogress / 600), 1)), () -> Pal.powerBar, () -> h.Hprogress));
     }
-
     public class heatMultiBuild extends MultiCrafterBuild{
-        public float heat, process = 0;
+        public float heat, Hprogress = 0;
         /* default crafting time */
         public float defCraftTime = 600f;
         public ItemStack[] inputStack;
         public int heatCap = 1;
+        public float boostScale = 1f;
         public int recLen = recs.length;
         public boolean isheated;
         public int sad;
-
-
 
         @Override
         public void updateTile(){
@@ -54,11 +51,17 @@ public class heatMulti extends MultiCrafter {
                 if (heat >= 0){heat -= 0.0005f;}
             }
 
-            applyBoost((heat+2), 120f);
+            boostScale = heat * 3;
+            applyBoost(boostScale, 120f);
+            if (isheated) { Hprogress += 0.0024f; }
+            if (Hprogress == 600f) {
+                craftTime = 0.1f;
+                Hprogress = 0;
+            }
+            craftTime = 600f;
 
             sad += 1;
             Log.info(sad);
-
 
         }
     }
