75627af6c7e9b3443314eab6ca31064a09e6ba42
AshesKaiser <76803502+AshesKaiser-KR@users.noreply.github.com>
2021-05-23 16:47:19 +0900

    h

--- src/example/world/blocks/heatMulti.java
+++ src/example/world/blocks/heatMulti.java
@@ -2,11 +2,11 @@ package example.world.blocks;
 
 import arc.Core;
 import arc.graphics.Color;
+import arc.util.Log;
 import arc.util.Strings;
 import mindustry.graphics.Pal;
 import mindustry.type.ItemStack;
 import mindustry.ui.Bar;
-import mindustry.world.modules.ItemModule;
 import multilib.MultiCrafter;
 import multilib.Recipe;
 
@@ -23,13 +23,11 @@ public class heatMulti extends MultiCrafter {
         super.setBars();
         bars.add("craftHeat", (heatMulti.heatMultiBuild e) -> new Bar(() -> Core.bundle.format("bar.craftHeat", Strings.fixed(e.heat * 400, 1)), () -> Pal.lightishOrange, () -> e.heat));
         bars.add("Calorie", (heatMulti.heatMultiBuild f) -> new Bar("bar.fuelBurn", Color.crimson, () -> f.calorie));
-        bars.add("craftProgress", (heatMulti.heatMultiBuild h) -> new Bar(() -> Core.bundle.format("bar.craftProgress", Strings.fixed(h.Hprogress, 1)), () -> Pal.powerBar, () -> h.Hprogress));
+        bars.add("craftProgress", (heatMulti.heatMultiBuild h) -> new Bar(() -> Core.bundle.format("bar.craftProgress", Strings.fixed(h.progress, 1)), () -> Pal.powerBar, () -> h.progress));
     }
 
     public class heatMultiBuild extends MultiCrafterBuild{
         public float heat = 0;
-        public ItemModule inItems;
-        public float Hprogress = 0;
         /* list of items in input */
         public ItemStack[] inputStack;
         /* amount and item name of fuel */
@@ -41,38 +39,33 @@ public class heatMulti extends MultiCrafter {
         /* how many recipes in that factory */
         public int recLen = recs.length;
         /* to increase heat or not */
-        public boolean isheated;
+        public boolean isHeated;
         /* calorie */
         public float calorie;
 
         @Override
         public void updateTile(){
             super.updateTile();
-            inItems = this.items;
             for (int i = 1; i < recLen; i++){
                 inputStack = recs[i].input.items;
                 fuelStack = inputStack[0];
-                isheated = items.has(inputStack);
-                if (isheated) break;
+                isHeated = items.has(inputStack);
+                if (isHeated) break;
             }
             if (items.has(fuelStack.item, fuelStack.amount)) {
                 if (calorie <= 0) {
                     calorie += 1f;
-                    inItems.remove(fuelStack.item, 1);
+                    items.remove(fuelStack.item, 1);
                 }
             }
-            if (isheated && heat <= heatCap) {
+            if (isHeated && heat <= heatCap) {
                 heat += 0.0005f;
             }else if (heat >= 0) heat -= 0.0005f;
-            calorie -= 0.0003f;
+            if (calorie >= 0) calorie -= 0.0003f;
             boostScale = heat * 3;
-            applyBoost(boostScale, 1f);
-            for (int j = 1; j < recLen; j++){
-                for (int k = 0; k < recs[j].output.items.length; k++){
-                    if (inItems.has(recs[j].output.items) && inItems.has (fuelStack.item, itemCapacity)) inItems.add (fuelStack.item, 1);
-                }
-            }
-            Hprogress = this.progress;
+            applyBoost(boostScale, 2f);
+            Log.info(progress);
+            if (progress >= 0.99845f) this.items.add(fuelStack.item, 1);
 
         }
     }
