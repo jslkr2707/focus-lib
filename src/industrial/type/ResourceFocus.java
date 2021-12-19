346b1fe182de7c7d2b15e133bf55f6065a9a1e00
AshesKaiser <jasonkorea0703lee@gmail.com>
2021-12-19 18:06:21 +0900

    how

--- src/industrial/type/ResourceFocus.java
+++ src/industrial/type/ResourceFocus.java
@@ -1,11 +1,13 @@
 package industrial.type;
 
 import arc.util.*;
+import mindustry.core.UI;
 import mindustry.ctype.ContentType;
 import mindustry.type.ItemStack;
 import mindustry.ui.dialogs.ResearchDialog;
 
 public class ResourceFocus extends Focus{
+    public static ResearchDialog researchDialog = new ResearchDialog();
     public ItemStack[] toAdd;
 
     public ResourceFocus(String name){
@@ -15,8 +17,9 @@ public class ResourceFocus extends Focus{
     @Override
     public void onUnlock(){
         for (ItemStack stack: this.toAdd){
-            new ResearchDialog().items.add(stack.item, stack.amount);
+            researchDialog.items.add(stack.item, stack.amount);
         }
+
     }
 
     @Override
