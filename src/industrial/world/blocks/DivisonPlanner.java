80a2664608adfb493578af85489a2de765b4e42e
AshesKaiser <jasonkorea0703lee@gmail.com>
2022-01-16 21:29:22 +0900

    ui update
    h

--- src/industrial/world/blocks/DivisonPlanner.java
+++ src/industrial/world/blocks/DivisonPlanner.java
@@ -5,6 +5,7 @@ import arc.graphics.Color;
 import arc.graphics.g2d.TextureRegion;
 import arc.scene.style.TextureRegionDrawable;
 import arc.scene.ui.Image;
+import arc.scene.ui.Label;
 import arc.scene.ui.layout.Table;
 import arc.struct.Seq;
 import arc.util.Scaling;
@@ -20,6 +21,7 @@ import mindustry.world.Block;
 public class DivisonPlanner extends Block {
     BaseDialog planDialog;
     public int combatWidth = 5;
+    public int remaining = 5;
     public Seq<UnitType> compose = new Seq<>(3);
 
     public DivisonPlanner(String name) {
@@ -29,8 +31,15 @@ public class DivisonPlanner extends Block {
         configurable = true;
         solid = true;
         logicConfigurable = true;
+        sync = true;
 
         compose.add(null, null, null);
+
+        config(UnitType.class, (DivisionPlannerBuild tile, UnitType unit) -> {
+            if (tile.remain()){
+                addCompose(tile.lastSelected, tile.selected);
+            }
+        });
     }
 
     public void addCompose(int i, UnitType unit){
@@ -41,11 +50,17 @@ public class DivisonPlanner extends Block {
     public class DivisionPlannerBuild extends Building{
         public UnitType selected;
         public Seq<UnitType> available = Vars.content.units().select(u -> !u.isBanned() && !u.isHidden() & u.node() != null);
+        public boolean pending = false;
+        public int lastSelected;
+
+        @Override
+        public void updateTile(){
+            super.updateTile();
+        }
 
         @Override
         public void buildConfiguration(Table table){
-            int col = 4;
-
+            super.buildConfiguration(table);
             if (planDialog == null){
                 planDialog = new BaseDialog("Division Planner");
                 planDialog.addCloseButton();
@@ -57,7 +72,7 @@ public class DivisonPlanner extends Block {
             planDialog.cont.table(t -> {
                 t.center();
 
-                t.add(Core.bundle.get("ui.combatwidth") + ": " + combatWidth);
+                t.add(Core.bundle.get("ui.combatwidth") + ": " + combatWidth).padBottom(20);
             });
 
             planDialog.cont.row();
@@ -65,41 +80,63 @@ public class DivisonPlanner extends Block {
             planDialog.cont.table(tt -> {
                 tt.center();
 
-                tt.button(new TextureRegionDrawable(btnImg(0)), () -> {
-                }).size(50).pad(5);
+                tt.button(new TextureRegionDrawable(btnImg(0, compose)), () -> {
+                    if (pending && selected != null){
+                        lastSelected = 0;
+                        pending = false;
+                        configure(selected);
+                    }
+                }).size(70).padRight(50f);
 
-                tt.button(new TextureRegionDrawable(btnImg(1)), () -> {
-                }).size(50).pad(5);
+                tt.button(new TextureRegionDrawable(btnImg(1, compose)), () -> {
+                    if (pending && selected != null){
+                        lastSelected = 1;
+                        pending = false;
+                        configure(selected);
+                    }
+                }).size(70).pad(5);
 
-                tt.button(new TextureRegionDrawable(btnImg(2)), () -> {
-                }).size(50).pad(5);
+                tt.button(new TextureRegionDrawable(btnImg(2, compose)), () -> {
+                    if (pending && selected != null){
+                        lastSelected = 2;
+                        pending = false;
+                        configure(selected);
+                    }
+                }).size(70).padLeft(50f);
 
+                tt.row();
+
+                tt.add(unitName(0)).padRight(50f);
+                tt.add(unitName(1)).pad(5);
+                tt.add(unitName(2)).padLeft(50f);
+            });
+
+            planDialog.cont.table(tbl -> {
+                tbl.left();
+
+                for (int i = 0;i < available.size;i++){
+                    int j = i;
+                    tbl.button(new TextureRegionDrawable(btnImg(i, available)), () -> {
+                        selected = available.get(j);
+                        pending = true;
+                    }).size(60).pad(10);
+                    if (i % 8 == 7) tbl.row();
+                }
             });
 
             planDialog.show();
         }
 
-        public TextureRegion btnImg(int i){
-            return compose.get(i) != null ? compose.get(i).uiIcon : Icon.none.getRegion();
+        public TextureRegion btnImg(int i, Seq<UnitType> seq){
+            return seq.get(i) != null ? seq.get(i).uiIcon : Icon.none.getRegion();
         }
 
-        public void select(Table t){
-            t.row();
+        public Label unitName(int i){
+            return compose.get(i) != null ? new Label(compose.get(i).localizedName) : new Label(Core.bundle.get("ui.none"));
+        }
 
-            for (int i = 0;i < available.size;i++){
-                UnitType unit = available.get(i);
-                t.button(b -> {
-                    b.left();
-
-                    b.image(unit.unlocked() ? unit.uiIcon : Icon.lock.getRegion()).size(40);
-
-                }, () -> {}).left();
-
-                if (i % 8 == 7){
-                    t.row();
-                }
-            }
-
+        public boolean remain(){
+            return remaining > 0;
         }
     }
 }
