b3b5d325c930de4ba9d493289a4d60f9d603d7bb
AshesKaiser <jasonkorea0703lee@gmail.com>
2022-06-05 21:23:44 +0900

    hhhhhhhhhhhhhhh

---
+++ src/ages/AgesVars.java
@@ -0,0 +1,17 @@
+package ages;
+
+import arc.assets.Loadable;
+
+public class AgesVars implements Loadable {
+    // region health
+    public static float blockHealthMulti = 1f;
+    public static float unitHealthMulti = 1f;
+
+    // region damage
+    public static float damageMulti = 1f;
+
+    //region crafter
+    public static float heatCapMulti = 1f;
+    public static int fuelUseMulti = 1;
+    public static float fuelCapMulti = 1f;
+}
--- src/ages/world/blocks/ancient/AncientCrafter.java
+++ src/ages/world/blocks/ancient/AncientCrafter.java
@@ -1,6 +1,7 @@
 package ages.world.blocks.ancient;
 
 import arc.*;
+import arc.math.Mathf;
 import arc.util.io.*;
 import mindustry.content.*;
 import mindustry.entities.*;
@@ -11,12 +12,15 @@ import mindustry.ui.*;
 import mindustry.world.blocks.production.*;
 import mindustry.world.meta.*;
 
+import static ages.AgesVars.*;
+
 public class AncientCrafter extends GenericCrafter{
     protected Item fuel;
     protected int fuelUse = 1;
     protected int fuelCapacity = 15;
     protected Effect fuelBurnEffect = Fx.smoke;
     protected float heatCapacity = 600f;
+    protected float minCraftHeat = 100f;
 
     public AncientCrafter(String name) {
         super(name);
@@ -36,40 +40,49 @@ public class AncientCrafter extends GenericCrafter{
         super.setBars();
 
         bars.add(Core.bundle.format("fuel"), (AncientCrafterBuild e) -> new Bar("ages.fuel", Pal.accent, e::heatf));
+        bars.add(Core.bundle.format("heat"), (AncientCrafterBuild e) -> new Bar("ages.heat", Pal.heal, e::heathf));
     }
 
     public class AncientCrafterBuild extends GenericCrafterBuild{
         protected float heat = 0;
+        protected float heath = 0;
 
         @Override
         public boolean consValid(){
-            return super.consValid() && (hasFuel() || heat > 0);
+            return super.consValid() && (hasFuel() || heath >= minCraftHeat);
         }
 
         @Override
-        public boolean shouldConsume(){ return super.shouldConsume() && (hasFuel() || heat > 0); }
+        public boolean shouldConsume(){ return super.shouldConsume() && (hasFuel() || heath >= minCraftHeat); }
 
         @Override
-        public boolean acceptItem(Building source, Item item){ return super.acceptItem(source, item) || (item == fuel && items.get(fuel) < fuelCapacity); }
+        public boolean acceptItem(Building source, Item item){ return super.acceptItem(source, item) || (item == fuel && items.get(fuel) < fuelCapacity * fuelCapMulti); }
 
         @Override
         public void updateTile(){
             super.updateTile();
 
             if (hasFuel() && heat <= 0 && consValid()){
-                items.remove(fuel, fuelUse);
-                heat = heatCapacity;
+                items.remove(fuel, fuelUse * fuelUseMulti);
+                heat = heatCapacity * heatCapMulti;
                 fuelBurnEffect.at(this);
+                if (heath < 1f){
+                    heath = Mathf.lerpDelta(heath, heatCapacity * heatCapMulti, 0.02f);
+                }
             }
 
             if (heat > 0){
                 heat -= 1;
+            }else{
+                heath -= 1;
             }
         }
 
         public boolean hasFuel(){ return items.get(fuel) > 0; }
 
-        public float heatf(){ return heat / heatCapacity; }
+        public float heatf(){ return heat / (heatCapacity * heatCapMulti); }
+
+        public float heathf(){ return heath / (heatCapacity * heatCapMulti); }
 
         @Override
         public void write(Writes write){
